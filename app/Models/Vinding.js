760f9f018b990f615ecfa076a5aad02974e9fc1e
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-16 00:44:10 -0600

    repair

---
+++ app/Models/Vinding.js
@@ -0,0 +1,19 @@
+//NOTE class is the blueprint of an object
+export default class Vmaching {
+    /**
+     * 
+     * @param {String} name 
+     * @param {Number} price 
+     * @param {Number} quantity 
+     * @param {URL} image 
+     */
+    //NOTE constructor is used to add the property for a created class 
+    constructor(name, price, qty, img) {
+        // this is used to refer to the object that it belong 
+        this.name = name
+        this.price = print
+        this.quantity = qty
+        this.image = img
+    }
+
+}
