41f90e9a415598b417c1870b7eeb86b1a52e326c
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-16 00:54:04 -0600

    Temp

--- app/Models/Vinding.js
+++ app/Models/Vinding.js
@@ -15,5 +15,15 @@ export default class Vmaching {
         this.quantity = qty
         this.image = img
     }
-
+    get Template() {
+        return
+        `<div class=" col text-center pr-5 mt-5 pt-5">
+        <img class="img-fluid" src="./assets/img/Red b.jpg" width="180" height="300"
+            alt="what for picture to load">
+        <h5>${this.Name}: <span> 0 </span> </h5>
+        <h5> ${this.Price}: <span> 0 </span> </h5>
+        <h5>${this.Quantity}: <span> 0 </span> </h5>
+        <h5>${this.Total}: <span> 0 </span> </h5>
+    </div>`
+    }
 }
